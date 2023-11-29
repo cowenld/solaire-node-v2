61203c11a3a3e485d0219669c964d90b72b53c94
Christopher Dyke <cowenld@gmail.com>
2023-11-29 10:05:40 +0000

    Add procfile

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,12 +2,16 @@ import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ApiTags } from '@nestjs/swagger';
 
+// this is the control hub, this get should be the websocket opening
+// I should open a connection after the movie is started ie - prisoner of azkhban
+// Then a websocket channel should be open for the user, or just generally subbed to the channel anyway
+
 @Controller()
 @ApiTags('solaire')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('/pause')
   pauseNetflix() {
     this.appService.pauseNetflix();
     return { message: 'Pausing netflix' };
